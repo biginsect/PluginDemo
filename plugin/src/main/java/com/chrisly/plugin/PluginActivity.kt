719fa14bef2bb8c77577e0ca1c2b091d223a5a0e
[biginsect] <[love-2008-dodo@163.com]>
2019-05-28 19:34:45 +0800

    init

---
+++ plugin/src/main/java/com/chrisly/plugin/PluginActivity.kt
@@ -0,0 +1,12 @@
+package com.chrisly.plugin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PluginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plugin)
+    }
+}
